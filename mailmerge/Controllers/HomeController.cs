5fc9b37b4197cafdcf46317d90b915140d26cc6c
Dennis Huillca <dennisbot@gmail.com>
2019-08-14 19:06:26 -0500

    Add syncfusion mailmerge sample

--- mailmerge/Controllers/HomeController.cs
+++ mailmerge/Controllers/HomeController.cs
@@ -5,6 +5,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using mailmerge.Models;
+using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+using System.IO;
 
 namespace mailmerge.Controllers
 {
@@ -38,6 +41,24 @@ namespace mailmerge.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public IActionResult CreateDocument()
+        {
+            //Opens the Word template document
+            FileStream fileStreamPath = new FileStream(@"Data/Letter Formatting.docx", FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            using (WordDocument document = new WordDocument(fileStreamPath, FormatType.Docx))
+            {
+                string[] fieldNames = { "ContactName", "CompanyName", "Address", "City", "Country", "Phone" };
+                string[] fieldValues = { "Nancy Davolio", "Syncfusion", "507 - 20th Ave. E.Apt. 2A", "Seattle, WA", "USA", "(206) 555-9857-x5467" };
+                //Performs the mail merge
+                document.MailMerge.Execute(fieldNames, fieldValues);
+                //Saves the Word document to MemoryStream
+                MemoryStream stream = new MemoryStream();
+                document.Save(stream, FormatType.Docx);
+                stream.Position = 0;
+                //Download Word document in the browser
+                return File(stream, "application/msword", "Result.docx");
+            }
         }
     }
 }
