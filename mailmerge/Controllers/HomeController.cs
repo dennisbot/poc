5a7de7f80ea305a32bf9169f041f7653c962e034
Dennis Huillca <dennisbot@gmail.com>
2019-09-04 15:59:35 -0500

    Add getruta to test unix paths

--- mailmerge/Controllers/HomeController.cs
+++ mailmerge/Controllers/HomeController.cs
@@ -46,17 +46,29 @@ namespace mailmerge.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public ActionResult getruta()
+        {
+            string cad = Path.Combine("hola", "mundo", "probando");
+            return Content(cad);
+        }
+
         public IActionResult CreateDocument()
         {
             //Opens the Word template document
-            FileStream fileStreamPath = new FileStream(@"wwwroot/Letter.docx", FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            FileStream fileStreamPath = new FileStream(@"wwwroot/test_template.docx", FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
 
             using (WordDocument document = new WordDocument(fileStreamPath, FormatType.Docx))
             {
-                string[] fieldNames = { "ContactName", "CompanyName", "Address", "City", "Country", "Phone" };
-                string[] fieldValues = { "Nancy Davolio", "Syncfusion", "507 - 20th Ave. E.Apt. 2A", "Seattle, WA", "USA", "(206) 555-9857-x5467" };
+                //string[] fieldNames = { "ContactName", "CompanyName", "Address", "City", "Country", "Phone" };
+                //string[] fieldValues = { "Nancy Davolio", "Syncfusion", "507 - 20th Ave. E.Apt. 2A", "Seattle, WA", "USA", "(206) 555-9857-x5467" };
+
+                string[] fieldNames = { "firstname", "lastname", "address", "age"};
+                string[] fieldValues = { "Dennis", "Huillca", "APV Agua Buena D-1 San Sebastian", "30" };
+                string[] fieldValues2 = { "Roy", "Palacios", "Surco Lima", "31" };
                 //Performs the mail merge
                 document.MailMerge.Execute(fieldNames, fieldValues);
+                document.MailMerge.Execute(fieldNames, fieldValues2);
 
                 //Converts Word document to PDF
                 DocIORenderer render = new DocIORenderer();
